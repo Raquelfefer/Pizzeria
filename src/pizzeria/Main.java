bd2e70958df2dd6c1c4509a5caaf1adb31b6f7ce
alvarosanchezz <alvsanrod2006@gmail.com>
2025-01-30 13:41:34 +0100

    actualizacion

---
+++ src/pizzeria/Main.java
@@ -0,0 +1,10 @@
+package pizzeria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pizza
+
+	}
+
+}
--- src/pizzeria/Pizzeria.java
+++ src/pizzeria/Pizzeria.java
@@ -4,19 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pizzeria {
-	private static List<Cliente> clientes = new ArrayList<>();
+
+	private List<Cliente> clientes = new ArrayList<>();
 	private static List<Pizza> pizzas = new ArrayList<>();
-	private static List<Pedido> pedidos = new ArrayList<>();
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public static void registrarPizzas(Pizza pizza) {
+		if (pizza == null) {
+			throw new IllegalArgumentException("La pizza no puede ser nula.");
+		}
 		pizzas.add(pizza);
 	}
 
-	public static void registrarClientes(Cliente cliente) {
+	public void registrarClientes(Cliente cliente) {
+		if (cliente == null) {
+			throw new IllegalArgumentException("El cliente no puede ser nulo.");
+		}
 		clientes.add(cliente);
 	}
 
-	public static void realizarPedido(Pedido pedido) {
+	public void realizarPedido(Pedido pedido) {
+		if (pedido == null) {
+			throw new IllegalArgumentException("El pedido no puede ser nulo.");
+		}
 		pedidos.add(pedido);
+	}
+	
+	public void mostrarPizzas() {
+		System.out.println(pizzas.toArray());
 	}
 }
