0682d9a1107e2049d722050ee58c0bc49d29fe4f
speicon1911 <salvadorpeinado111906@gmail.com>
2025-01-30 12:37:24 +0100

    Actualizacion de Clase Pedido

--- src/pizzeria/Pedido.java
+++ src/pizzeria/Pedido.java
@@ -1,29 +1,54 @@
 package pizzeria;
 
+import java.time.LocalDateTime;
+
+import pizzeria.enums.EnumTipo;
+
 public class Pedido {
 	private static int contadorIdPedido;
 	private int idPedido;
-	private String cliente;
-	private String fecha;
+	private Cliente cliente;
+	private LocalDateTime fecha;
 	private double total;
-	private TipoPedido tipo;
-	
-	public Pedido(String cliente, String fecha, double total, TipoPedido tipo) {
+	private EnumTipo tipo;
+
+	public Pedido(String cliente, double total, EnumTipo tipo) {
 		contadorIdPedido++;
 		this.idPedido = contadorIdPedido;
-		
+		setCliente(cliente);
+		this.fecha = LocalDateTime.now();
+		setTotal(total);
 	}
-	
+
 	public void setCliente(String cliente) {
-		if(cliente == null || cliente.isBlank()) {
+		if (cliente == null || cliente.isBlank()) {
 			throw new IllegalArgumentException("El nombre del cliente no puede ser nulo o estar vacio");
-		}else {
-		this.cliente = cliente;
-	}
+		} else {
 		}
-	public String getCliente() {
+	}
+
+	public Cliente getCliente() {
 		return cliente;
 	}
+
+	public int getIdPedido() {
+		return idPedido;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public double getTotal() {
+		return total;
+	}
 	
+	public EnumTipo getTipo() {
+		return tipo;
+	}
 	
 }
