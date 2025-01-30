3f3656cd0b265128f9c609fae25bcbc9953b2b4a
alvarosanchezz <alvsanrod2006@gmail.com>
2025-01-30 14:24:44 +0100

    actualizacion menu

--- src/pizzeria/Main.java
+++ src/pizzeria/Main.java
@@ -1,10 +1,20 @@
 package pizzeria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		menu();
+	}
 
+	public static void menu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("1. Nueva pizza \n2. Nuevo cliente \n3. Realizar un pedido \n4. Ver pizzas "
+				+ "\n5. Ver pedidos \n6. Ver clientes \n7. Consultar cliente mediante su telefono"
+				+ "\n8. Buscar pizzas con ingrediente \n9. Buscarpizza sin ingrediente \n10. Pedidos de hoy"
+				+ "\n11. Pedidos consumidos en el local \n12. Pedidos para recoger \n13. Pedidos a domicilio"
+				+ "\n14. Pizza mas cara \n15. Pizza mas barata \n16. Ultimo pedido realizado \n17. Salir");
 	}
 
 }
