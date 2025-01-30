8f2b853afc774b807ef030c48e20e6e1abfd303e
Raquelfefer <raquelfefer@gmail.com>
2025-01-30 13:58:36 +0100

    mostrar pizzas

--- src/pizzeria/Main.java
+++ src/pizzeria/Main.java
@@ -3,7 +3,7 @@ package pizzeria;
 public class Main {
 
 	public static void main(String[] args) {
-		Pizza
+	
 
 	}
 
--- src/pizzeria/Pizzeria.java
+++ src/pizzeria/Pizzeria.java
@@ -4,33 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pizzeria {
-
-	private List<Cliente> clientes = new ArrayList<>();
+	private static List<Cliente> clientes = new ArrayList<>();
 	private static List<Pizza> pizzas = new ArrayList<>();
-	private List<Pedido> pedidos = new ArrayList<>();
+	private static List<Pedido> pedidos = new ArrayList<>();
 
 	public static void registrarPizzas(Pizza pizza) {
-		if (pizza == null) {
-			throw new IllegalArgumentException("La pizza no puede ser nula.");
-		}
 		pizzas.add(pizza);
 	}
 
-	public void registrarClientes(Cliente cliente) {
-		if (cliente == null) {
-			throw new IllegalArgumentException("El cliente no puede ser nulo.");
-		}
+	public static void registrarClientes(Cliente cliente) {
 		clientes.add(cliente);
 	}
 
-	public void realizarPedido(Pedido pedido) {
-		if (pedido == null) {
-			throw new IllegalArgumentException("El pedido no puede ser nulo.");
-		}
+	public static void realizarPedido(Pedido pedido) {
 		pedidos.add(pedido);
 	}
 	
 	public void mostrarPizzas() {
-		System.out.println(pizzas.toArray());
+		System.out.println("Lista de pizzas:");
+		for (Pizza pizza: pizzas) {
+			System.out.println(pizza.getNombre());
+		}
 	}
 }
