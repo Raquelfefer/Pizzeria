da683fa466aeb1e8ea2777d8e8edb54b706fcd12
speicon1911 <salvadorpeinado111906@gmail.com>
2025-02-04 12:33:48 +0100

    Update Pizzeria.java

--- src/pizzeria/Pizzeria.java
+++ src/pizzeria/Pizzeria.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pizzeria {
-	
+
 	private List<Cliente> clientes;
 	private List<Pizza> pizzas;
 	private List<Pedido> pedidos;
-	
+
 	public Pizzeria() {
 		this.clientes = new ArrayList<Cliente>();
 		this.pizzas = new ArrayList<Pizza>();
@@ -25,7 +25,6 @@ public class Pizzeria {
 		pizzas.add(pizza);
 	}
 
-	
 	public void registrarClientes(Cliente cliente) {
 		if (cliente == null) {
 			throw new IllegalArgumentException("El cliente no puede ser nulo.");
@@ -33,35 +32,35 @@ public class Pizzeria {
 		clientes.add(cliente);
 	}
 
-	
 	public void realizarPedido(Pedido pedido) {
 		if (pedido == null) {
 			throw new IllegalArgumentException("El pedido no puede ser nulo.");
 		}
 		pedidos.add(pedido);
 	}
-	
+
 	public void mostrarPizzas() {
 		System.out.println("Lista de pizzas:");
-		for (Pizza pizza: this.pizzas) {
-			pizza.mostrarInfoPizza();;
+		for (Pizza pizza : this.pizzas) {
+			pizza.mostrarInfoPizza();
+			;
 		}
 	}
-	
+
 	public void mostrarPedidos() {
 		System.out.println("Lista de pedidos:");
 		for (Pedido pedido : this.pedidos) {
-			pedido.mostrarInfoPedido();;
+			pedido.mostrarInfoPedido();
+			;
 		}
 	}
-	
+
 	public void mostrarClientes() {
 		System.out.println("Lista de clientes:");
-		for (Cliente cliente: this.clientes) {
+		for (Cliente cliente : this.clientes) {
 			cliente.mostrarInfoCliente();
 		}
 	}
-	
 
 	public void consultarClienteTelefono(String telefono) {
 		for (Cliente cliente : this.clientes) {
@@ -71,56 +70,61 @@ public class Pizzeria {
 		}
 	}
 
-
 	public void pizzasConIngrediente(String ingrediente) {
-		if(ingrediente == null || ingrediente.isBlank()) {
+		if (ingrediente == null || ingrediente.isBlank()) {
 			throw new IllegalArgumentException("El ingrediente no puede ser nulo o en blanco.");
 		}
 		System.out.println("Estas pizzas tienen " + ingrediente);
-		for (Pizza pizza: this.pizzas) {
-			if(pizza.getIngredientes().contains(ingrediente)) {
+		for (Pizza pizza : this.pizzas) {
+			if (pizza.getIngredientes().contains(ingrediente)) {
 				System.out.println(pizza.getNombre());
-			}
-			else{
+			} else {
 				System.out.println("No hay pizzas con este ingrediente.");
 			}
 		}
 	}
-	
+
 	public void pizzasSinIngrediente(String ingrediente) {
-		if(ingrediente == null || ingrediente.isBlank()) {
+		if (ingrediente == null || ingrediente.isBlank()) {
 			throw new IllegalArgumentException("El ingrediente no puede ser nulo o en blanco.");
 		}
 		System.out.println("Estas pizzas no tienen " + ingrediente);
 		for (Pizza pizza : this.pizzas) {
-			if(!(pizza.getIngredientes().contains(ingrediente))) {
+			if (!(pizza.getIngredientes().contains(ingrediente))) {
 				System.out.println(pizza.getNombre());
-			}
-			else {
+			} else {
 				System.out.println("No hay pizzas con estas características.");
 			}
 		}
 	}
 
-	
 	public void consultarPedidosLocal() {
 		System.out.println("Los pedidos consumidos en local son: ");
-		for(Pedido pedido : this.pedidos) {
-			if(pedido.getTipo().equals("LOCAL")) {
+		for (Pedido pedido : this.pedidos) {
+			if (pedido.getTipo().equals("LOCAL")) {
 				pedido.mostrarInfoPedido();
 			}
 		}
 	}
-	
+
 	public void consultarPedidosRecoger() {
 		System.out.println("Los pedidos para recoger son: ");
-		for(Pedido pedido: this.pedidos) {
-			if(pedido.getTipo().equals("RECOGER")) {
+		for (Pedido pedido : this.pedidos) {
+			if (pedido.getTipo().equals("RECOGER")) {
 				pedido.mostrarInfoPedido();
 			}
 		}
 	}
-	
+
+	public void consultarPedidosDomicilio() {
+		System.out.println("Los pedidos a domicilio son: ");
+		for (Pedido pedido : pedidos) {
+			if (pedido.getTipo().equals("DOMICILIO")) {
+				pedido.mostrarInfoPedido();
+			}
+		}
+	}
+
 	public void consultarPedidosHoy() {
 		LocalDateTime inicio = LocalDateTime.of(LocalDate.now(), LocalTime.NOON);
 		LocalDateTime fin = LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.MIDNIGHT);
@@ -131,8 +135,5 @@ public class Pizzeria {
 
 		}
 	}
-	
-	
 
-	
 }
